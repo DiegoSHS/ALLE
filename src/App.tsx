62e755054bda21e4844d3fa471cb9f490695223a
Brian Hoyos Leal <brianthesfkjuly@gmail.com>
2025-06-08 19:59:17 -0600

    QR Generator added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 import { QrPage } from "./pages/qrvisor";
+import { QrGenerator } from "./features/qrgenerator";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
       <Route element={<QrPage></QrPage>} path="/qr"></Route>
+      <Route element={<QrGenerator></QrGenerator>} path="/gene"></Route>
     </Routes>
   );
 }
