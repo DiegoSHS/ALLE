1bcbf777590527d50af9fa7c659427675910360d
Diego López Ariza <diegoawesome2017@outlook.com>
2025-06-09 22:46:36 -0600

    New dynamic route added

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { StockPage } from "./pages/stock";
 import { QrPage } from "./pages/qrvisor";
 import { QrScanner } from "./features/codescanner";
 import { QrGenerator } from "./features/qrgenerator";
+import { DynamicStock } from "./features/stock/dynamic";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
       <Route element={<QrScanner />} path="/scan" />
+      <Route element={<DynamicStock />} path="/stock/:id" />
       <Route element={<StockPage />} path="/stock" />
       <Route element={<QrPage></QrPage>} path="/qr"></Route>
       <Route element={<QrGenerator></QrGenerator>} path="/gene"></Route>
--- src/pages/stock.tsx
+++ src/pages/stock.tsx
@@ -1,21 +1,41 @@
 import { StockCard } from "@/features/stock";
 
-const cards = (value: number) => {
+export interface Stock {
+    id: number;
+    name: string;
+    description: string;
+}
+
+interface StockCardsProps {
+    items: Stock[]
+}
+
+const StockCards = ({ items }: StockCardsProps) => {
     return (
-        <StockCard key={value} />
+        <>
+            {
+                items.map((item) => {
+                    return (
+                        <StockCard key={item.id} {...item}></StockCard>
+                    )
+                })
+            }
+        </>
     )
 }
 
 export function StockPage() {
     const items = Array.from({ length: 8 }, (_, k) => {
-        return k
+        return {
+            id: k,
+            name: `Item ${k + 1}`,
+            description: `Description for item ${k + 1}`,
+        }
     })
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex flex-wrap gap-4">
-                {
-                    items.map(cards)
-                }
+                <StockCards items={items} />
             </div>
         </div>
     )
